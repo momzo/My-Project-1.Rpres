e01f2b494c7588757ca9036a63562ac3ed261585
momzo <momzo12@optimum.net>
2016-02-28 03:24:57 -0500

    Added files via upload

---
+++ gadgets.R
@@ -0,0 +1,24 @@
+
+
+library(shiny)
+
+get_password<-function(){
+  library(miniUI)
+  ui<-miniPage(
+    gadgetTitleBar("Please enter your password"),
+    miniContentPanel(
+      passwordInput("password", "")
+    )
+  )
+}
+
+server<-function(input,output){
+  observeEvent(input$done, {
+    stopApp(input$password)
+  })
+  observeEvent(input$cancel, {
+    stopApp(stop("No password.", call. = FALSE))
+  })
+  runGadget(ui,server)
+}
+
