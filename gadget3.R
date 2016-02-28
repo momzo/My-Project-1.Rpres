e54f7920f8936354fc1143679d40cfd1cb12d2c1
momzo <momzo12@optimum.net>
2016-02-28 15:48:48 -0500

    Added files via upload

---
+++ gadget3.R
@@ -0,0 +1,20 @@
+
+library(datasets)
+data("mtcars")
+library(shiny)
+
+ui<-basicPage(
+  plotOutput("plot1", click = "plot_click", width = 400),
+  verbatimTextOutput("info")
+)
+
+server<-function(input,output){
+  output$plot1<-renderPlot({
+    plot(mtcars$wt, mtcars$mpg)
+  })
+  
+  output$info<-renderText({
+    paste0("x =", input$plot_click$x, "\n",
+           "y =", input$plot_click$y)
+  })
+}
