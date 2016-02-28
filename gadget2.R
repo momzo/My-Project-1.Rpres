70f99a52713144bd40ce2b521085cd3e0dd622b0
momzo <momzo12@optimum.net>
2016-02-28 15:50:05 -0500

    Added files via upload

---
+++ gadget2.R
@@ -0,0 +1,13 @@
+
+library(shiny)
+ui<-basicPage(
+  plotOutput("plot"),
+  sliderInput("bins", "Number of bins:", 1, 50, 20)
+  
+)
+
+server<-function(input,output){
+  output$plot<-renderPlot({
+    hist(faithful$waiting, breaks = input$bins, col = "red")
+  })
+}
