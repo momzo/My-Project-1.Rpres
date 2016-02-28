12283fe693e240c46565104d96eb2d2d6a95ef08
momzo <momzo12@optimum.net>
2016-02-28 18:16:01 -0500

    Added files via upload

--- gadget6.R
+++ gadget6.R
@@ -2,18 +2,24 @@
 library(shiny)
 
 ui<-basicPage(
-  plotOutput("plot1", click = "plot_click", width = 400),
+  plotOutput("plot1", click = "plot_click", width = 400,
+             dblclick = dblclickOpts(id = "plot_dblclick"),
+             hover = hoverOpts(
+               id = "plot_hover", delay = 500),
+               brush = "plot_brush"
+             ),
+             
+            
   verbatimTextOutput("info"),
   actionButton("done","Done")
 )
 
 server<-function(input,output){
-  output$info<-renderPrint({
+ output$info<-renderPrint({
     row<-nearPoints(mtcars, input$plot_click,
                     xvar = "wt", yvar = "mpg",
                     threshold = 5, maxpoints = 1)
     cat("Nearest point within 5 pixels:\n")
-    print(row)
   })
 
   vals<-reactiveValues(mtc = mtcars[, c("wt","mpg")])
@@ -30,5 +36,11 @@ server<-function(input,output){
   
   observeEvent(input$done, {
     stopApp(vals$mtc)
+  })
+  output$info<-renderPrint({
+    rows<-brushedPoints(mtcars,input$plot_brush,
+                        xvar = "wt", yvar = "mpg")
+    cat("Brushed points:\n")
+    print(rows)
   })
 }
